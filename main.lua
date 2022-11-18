b6b8ee1140b1c92c18aa284ff68d1a9aabf5cb69
void <117407823+voidded@users.noreply.github.com>
2022-11-17 16:35:35 -0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -70,7 +70,7 @@ local Games = {
 	["Merge_Simulator"] = 10925589760,
 	["Broken_Bones_IV"] = 2551991523,
     ["Race_Clicker"] = 9285238704,
-    ["Cheese_Escape"] = 5777099015,
+    ["Arsenal"] = , -- DO (will be free)
 }
 
 
@@ -354,6 +354,451 @@ if game.PlaceId == Games.Race_Clicker then -- NOT DONE (void making)
 })
 end
 
-if game.PlaceId == Games.Cheese_Escape then -- would like to do (WIP)
-    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = -279, 7, -15
-end
+if game.PlaceId == Games.Arsenal then
+
+	local aimbotOn
+
+	local ArsenalTab = Window:CreateTab("Aim")
+
+	local s1 = ArsenalTab:CreateSection("Aimbot")
+	local Aimbot = ArsenalTab:CreateToggle({
+		Name = "Aimbot",
+		CurrentValue = false,
+		Flag = "Toggle2",
+		Callback = function(Value)
+		_G.AimbotInput = "RightClick"
+		_G.AimbotEasing = 0.2
+		_G.TeamCheck = true
+		if _G.AlreadyRanItBroDanger then
+			return
+		end
+	_G.AlreadyRanItBroDanger = "It's your nutshack"
+
+	local uis = game:GetService("UserInputService")
+	local players = game:GetService("Players")
+	local teams = game:GetService("Teams")
+	local plr = players.LocalPlayer
+	local ms = plr:GetMouse()
+	local cam = workspace.CurrentCamera
+
+	workspace:GetPropertyChangedSignal("CurrentCamera"):Connect(function()
+		cam = Workspace.CurrentCamera
+	end)
+
+	local aimbotOn = false
+
+	function canSee(part, des)
+		local origin = cam.CFrame.Position
+		local __, onscreen = cam:WorldToViewportPoint(part.Position)
+
+		if onscreen then
+			local ray = Ray.new(origin, part.Position - origin)
+			local hit = workspace:FindPartOnRayWithIgnoreList(ray, plr.Character:GetDescendants())
+			local vis = false
+
+			if hit and hit:IsDescendantOf(des) then
+				vis = true
+			else
+				vis = false
+			end
+
+			return vis
+		else
+			return false
+		end
+	end
+
+	function getClosestPlrToMouse()
+		local closest, distance = nil, math.huge
+
+		for i, v in pairs(player:GetPlayers()) do
+			if v ~= plr then
+				pcall(function()
+					local checkPassed = true
+				
+					if _G.TeamCheck then
+						if plr.TeamColor == v.TeamColor then
+							checkPassed = false
+						end
+					end
+					
+					if (ms.Hit.Position - v.Character.PrimaryPart.Position).magnitude < distance and checkPassed then
+						if canSee(v.Character.Head, v.Character) and v.Character.Humanoid.Health > 0 then
+							closest = v
+							distance = (ms.Hit.Position - v.Charactter.PrimaryPart.Position).magnitude
+						end
+					end
+				end)
+			end
+		end
+
+		return closest, distance
+	end
+	uis.InputBegan:Connect(function(input, processed)
+		if not processed then
+			if _G.AimbotInput == "LeftClick" and input.UserInputType == Enum.UserInputType.MouseButton1 then
+				aimbotOn = true
+			elseif _G.AimbotInput == "RightClick" and input.UserInputType == Enum.UserInputType.MouseButton2 then
+				aimbotOn = true
+			elseif input.KeyCode.Name == _G.AimbotInput then
+				aimbotOn = true
+			end
+		end
+	end)
+
+	uis.InputEnded:Connect(function(input, processed)
+		if not processed then
+			if _G.AimbotInput == "LeftClick" and input.UserInputType == Enum.UserInputType.MouseButton1 then
+				aimbotOn = false
+			elseif _G.AimbotInput == "RightClick" and input.UserInputType == Enum.UserInputType.MouseButton2 then
+				aimbotOn = false
+			elseif input.KeyCode.Name == _G.AimbotInput then
+				aimbotOn = false
+			end
+		end
+	end)
+	
+	while task.wait() do
+		if aimbotOn then
+			local targ = getClosestPlrToMouse
+			if targ then
+				local currentcf = workspace.CurrentCamera.CFrame
+				workspace.CurrentCamera.CFrame = currentcf:Lerp(CFrame.new(currentcf.Position, targ.Character.Head.Position), _G.AimbotEasing)
+			end
+		end
+	end
+		end
+	})
+
+	local s2 = ArsenalTab:CreateSection("ESP")
+	local ESP = ArsenalTab:CreateToggle({
+		Name = "Box ESP",
+		CurrentValue = false,
+		Flag = "Toggle3",
+		Callback = function(Value)
+		local lplr = game.Players.LocalPlayer
+		local camera = game:GetService()
+		local CurrentCamera = workspace.CurrentCamera
+		local worldToViewportPoint = CurrentCamera.worldToViewportPoint
+
+		local HeadOff = Vector3.new(0, 0.5, 0)
+		local LegOff = Vector3.new(0, 3, 0)
+
+		for i,v in pairs(game.Players:GetChildren()) do
+			local BoxOutline = Drawing.new("Square")
+			BoxOutline.Visible = false
+			BoxOutline.Color = Color3.new(0,0,0)
+			BoxOutline.Thickness = 3
+			BoxOutline.Transparency = 1
+			BoxOutline.Filled = false
+
+			local Box = Drawing.new("Square")
+			Box.Visible = false
+			Box.Color = Color3.new(1,1,1)
+			Box.Thickness = 1
+			Box.Transparency = 1
+			Box.Filled = false
+
+			function boxesp()
+				game:GetService("RunService").RenderStepped:Connect(function()
+				if v.Character ~= nil and v.Character:FindFirstChild("Humanoid") ~= nil and v.Character:FindFirstChild("HumanoidRootPart") ~= nil and v ~= lplr and v.Character.Humanoid.Health > 0 then
+					local Vector, onScreen = camera:worldToViewportPoint(v.Character.HumanoidRootPart.Position)
+					
+					local RootPart = v.Character.HumanoidRootPart
+					local Head = v.Character.Head
+					local RootPosition, RootVis = worldToViewportPoint(CurrentCamera, RootPart.Position)
+					local HeadPosition = worldToViewportPoint(CurrentCamera, Head.Position + HeadOff)
+					local LegPosition = worldToViewportPoint(CurrentCamera, RootPart.Position - LegOff)
+					
+					if onScreen then
+						BoxOutline.Size = Vector2.new(1000 / RootPosition.Z, HeadPosition.Y - LegPosition.Y)
+						BoxOutline.Position = Vector2.new(RootPosition.X - BoxOutline.Size.X / 2, RootPosition.Y - BoxOutline.Size.Y / 2)
+						BoxOutline.Visible = true
+					
+						Box.Size = Vector2.new(1000 / RootPosition.Z, HeadPosition.Y - LegPosition.Y)
+						Box.Position = Vector2.new(RootPosition.X - BoxOutline.Size.X / 2, RootPosition.Y - BoxOutline.Size.Y / 2)
+						Box.Visible = true
+
+						if v.TeamColor == lplr.TeamColor then
+							BoxOutline.Visible = false
+							Box.Visible = false
+						else
+							BoxOutline.Visible = true
+							Box.Visible = true
+						end
+
+					else
+						BoxOutline.Visible = false
+						Box.Visible = false
+					end
+				else
+					BoxOutline.Visible = false
+					Box.Visible = false
+				end
+			end)
+		end
+		coroutine.wrap(boxesp)()
+	end
+
+	game.Players.PlayerAdded:Connect(function(v)
+	local BoxOutline = Drawing.new("Square")
+	BoxOutline.Visible = false
+	BoxOutline.Color = Color3.new(0,0,0)
+	BoxOutline.Thickness = 3
+	BoxOutline.Transparency = 1
+	BoxOutline.Filled = false
+
+	local Box = Drawing.new("Square")
+	Box.Visible = false
+	Box.Color = Color3.new(1,1,1)
+	Box.Thickness = 1
+	Box.Transparency = 1
+	Box.Filled = false
+
+	function boxesp()
+		game:GetService("RunService").RenderStepped:Connect(function()
+			if v.Character ~= nil and v.Character:FindFirstChild("Humanoid") ~= nil and v.Character:FindFirstChild("HumanoidRootPart") ~= nil and v ~= lplr and v.Character.Humanoid.Health > 0 then
+				local Vector, onScreen = camera:worldToViewportPoint(v.Character.HumanoidRootPart.Position)
+			
+				local RootPart = v.Character.HumanoidRootPart
+				local Head = v.Character.Head
+				local RootPosition, RootVis = worldToViewportPoint(CurrentCamera, RootPart.Position)
+				local HeadPosition = worldToViewportPoint(CurrentCamera, Head.Position + HeadOff)
+				local LegPosition = worldToViewportPoint(CurrentCamera, RootPart.Position - LegOff)
+				
+				if onScreen then
+					BoxOutline.Size = Vector2.new(1000 / RootPosition.Z, HeadPosition.Y - LegPosition.Y)
+					BoxOutline.Position = Vector2.new(RootPosition.X - BoxOutline.Size.X / 2, RootPosition.Y - BoxOutline.Size.Y / 2)
+					BoxOutline.Visible = true
+				
+					Box.Size Vector2.new(1000 / RootPosition.Z, HeadPosition.Y - LegPosition.Y)
+					Box.Position = Vector2.new(RootPosition.X - Box.Size.X / 2, RootPosition.Y - Box.Size.Y / 2)
+					Box.Visible = true
+					
+					if v.TeamColor == lplr.TeamColor then
+						BoxOutline.Visible = false
+						Box.Visible = false
+					else
+						BoxOutline.Visible = true
+						Box.Visible = true
+					end
+				
+				else
+					BoxOutline.Visible = false
+				Box.Visible = false
+				end
+			end)
+		end
+		coroutine.wrap(boxesp)()
+	end)
+		end	
+	})
+	local ESP2 = ArsenalTab:CreateToggle({
+		Name = "Name ESP",
+		CurrentValue = false,
+		Flag = "Toggle4",
+		Callback = function(Value)
+		local c = workspace.CurrentCamera
+		local ps = game:GetService("Players")
+		local lp = ps.LocalPlayer
+		local rs = game:GetService("RunService")
+
+		local function esp(p,cr)
+			local h = cr:WaitForChild("Humanoid")
+			local hrp = cr:WaitForChild("HumanoidRootPart")
+
+			local text = Drawing.new("Text")
+			text.visible = false
+			text.center = true
+			text.Outline = true
+			text.Font = 2
+			text.Color = Color3.fromRGB(255,255,255)
+			text.Size = 13
+
+			local c1
+			local c2
+			local c3
+
+			local function dc()
+				text.Visible = false
+				text:Remove()
+				if c1 then
+					c1:Disconnect()
+					c1 = nil
+				end
+				if c3 then
+					c3:Disconnect()
+					c3 = nil
+				end
+			end
+
+			c2 = cr.AncestryChanged:Connect(function(_,parent)
+				if not parent then
+					dc()
+				end
+			end)
+
+			c3 = h.HealthChanged:Connect(function(v)
+			if (v<=0) or (h:GetState() == Enum.HumanoidStateType.Dead) then
+				dc()
+			end
+		end)
+	end
+
+	local function p_added(p)
+		if p.Character then
+			esp(p,p.Character)
+		end
+		p.CharacterAdded:Connect(function(cr)
+		esp(p,cr)
+		end)
+	end
+
+	for i,p in next, ps:GetPlayers() do
+		if p ~= lp then
+			p_added(p)
+		end
+	end
+
+	ps.PlayerAdded:Connect(p_added)
+	end
+	})
+	local SilentAim = ArsenalTab:CreateToggle({
+		Name = "Silent Aimbot",
+		CurrentValue = false,
+		Flag = "Toggle5",
+		Callback = function(Value)
+		assert(getgc, "missing dependency: getgc");
+			
+		local players  = game:GetService("Players");
+		local workspace = game:GetService("Workspace");
+		local input_service = game:GetService("UserInputService");
+		local replicated_first = game:GetService("ReplicatedFirst");
+		
+		local camera = workspace.CurrentCamera;
+		local wtvp = camera.WorldToViewportPoint;
+		local mouse_pos = input_service.GetMouseLocation;
+		local localplayer = players.LocalPlayer;
+		local ticket = 0;
+
+		local new_vector2 = Vector2.new;
+
+		local modules = {};
+		modules.network = require(replicated_first.ClientModules.Old.framework.network);
+		modules.values = require(replicated_first.SharedModules.SharedConfigs.PublicSettings);
+		modules.physics = require(replicated_first.SharedModules.Old.Utilities.Math.physics:Clone());
+
+		for _, v in next, getgc(true) do
+			if type(v) == "table" then
+				if rawget(v, "getbodyparts") then
+					modules.replication = v;
+				elseif rawget(v, "gammo") then
+					modules.gamelogic = v;
+				end
+			end
+		end
+
+		local function get_closest()
+			local closest, player = math.huge, nil;
+			for _, p in next, players:GetPlayers() do
+				local character = modules.replication.getbodyparts(p);
+				if character and p.Team ~= localplayer.Team then
+					local pos, visible = wtvp(camera, character.head.Position);
+					pos = new_vector2(pos.X, pos.Y);
+
+					local magnitude = (pos - mouse_pos(input_service)).Magnitude;
+					if magnitude < closest and visible then
+						closest = magnitude;
+						player = p;
+					end
+				end
+			end
+			return player;
+		end
+
+		local old = modules.network.send;
+		function modules.network.send(self, name, ...)
+			local args = table.pack(...);
+			if name = "newbullets" then
+				local gun = modules.gamelogic.currnetgun;
+				local data = gun and gun.data;
+				if gun and data then
+					local player = get_closest();
+					local character = modules.replication.getbodyparts(player);
+					if player and character then
+						local hitpart = character.head;
+
+						for _, bullet in next, args[1].bullets do
+							bullet[1] = modules.physics.trajectory(args[1].firepos, modules.values.bulletAcceleration, hitpart.Position, data.bulletspeed);
+							bullet[2] = ticket;
+
+							ticket += 1;
+						end
+
+						old(self, name, table.unpack(args));
+
+						for _, bullet in next, args[1].bullets do
+							old(self, "bullethit", player, hitpart.Position, hitpart.Name, bullet[2]);
+						end
+
+						return;
+					end
+				end
+			end
+			return old(self, name, table.unpack(args));
+		end
+			end
+	})
+
+	local PlayerChams = ArsenalTab:CreateToggle({
+		Name = "Player Chams",
+		CurrentValue = false,
+		Flags = "Toggle6",
+		Callback = function()
+			local settings = {
+
+				fillcolor = Color3.fromRGB(255,255,255);
+				filltransparency = .75;
+				
+				outlinecolor = Color3.fromRGB(255, 235, 67);
+				outlinetransparency = 0;
+
+			}
+			
+			local plr = game:service'Players'.LocalPlayer
+			local highlights = Instance.new('Folder', game:service'CoreGui')
+
+			local function addhighlight(object)
+				local highlight = Instance.new('Highlight', highlights)
+				highlight.Adornee = object
+
+				highlight.FillColor = settings.fillcolor
+				highlight.FillTransparency = settings.filltransparency
+
+				highlight.OutlineColor = settings.outlinecolor
+				highlight.OutlineTransparency = settings.outlinetransparency
+
+				highlight.Adonee.Changed:Connect(function()
+				if not highlight.Adornee or not highlight.Adornee.Parent then
+					highlight:Destroy()
+				end
+			end)
+
+			return highlight
+		end
+
+		local function addtoplayer(object)
+			if object:IsA'Model' and object.Name == 'Player' and object.Parent.Name ~= tostring(plr.TeamColor) then
+				addhighlight(object)
+			end
+		end
+
+		for i,v in pairs(workspace.Players:GetDescendants()) do
+			addtoplayer(v)
+		end
+
+		workspace.Players.DescendantAdded:Connect(function(v)
+		addtoplayer(v)
+		end)
+			end
+	})
